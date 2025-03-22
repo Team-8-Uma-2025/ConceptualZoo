c7007ebc2ab8ac4a35deece491727863409c347e
Joshua Eworo <eworojoshua@gmail.com>
2025-03-21 20:37:31 -0500

    Switched bcrypt with bcryptjs to avoid binary deps

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Create express app
 const app = express();
