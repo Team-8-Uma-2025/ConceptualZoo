f4712d95a982e171322a4387dff82549c9c4dcbe
Jalvz1 <javieralvz0427@gmail.com>
2025-03-26 21:46:49 -0500

    blah blah blah

--- frontend/src/pages/AttractionDetails.js
+++ frontend/src/pages/AttractionDetails.js
@@ -3,14 +3,14 @@ import { useAuth } from '../context/AuthContext';
 import axios from 'axios';
 
 const AttractionDetails = () => {
-    const {id: urlEnclosureId} = useParams();
+    const {id: urlAttractionId} = useParams();
 
     /*States*/
     const {currentUser} = useAuth(); // user from authContext
     const [search_aID, setAttractionId] = useState(''); // search variable
     const [attractionList, setAttractionList] = useState([]); //preload attractions for manager functions
+    const [assignedAttractions, setAssignedAttractions] = useState([]); // for regular staff
     const [selectedAttraction, setSelectedAttraction] = useState(null); //fetch enclosure details
-
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null); 
 
@@ -28,6 +28,47 @@ const AttractionDetails = () => {
         Description: "",
         Picture: "",
     });
+
+    /* joshua code to refernece
+    useEffect(() => {
+        const fetchAssignedAttraction = async () => {
+            if(!currentUser) return;
+            try {
+                setLoading(true);
+                let endpoint = '/api/attraction';
+
+                // if user is a zookeeper, get their assigned attraction
+                if(currentUser.staffType === 'Zookeeper'){
+                    endpoint = `/api/attraction/staff/${currentUser.id}`;
+                }
+
+            } catch (err) {
+                console.error("Failed to fetch attraction", err);
+                setError("Failed to load your assigned enclosures. Please try again.");
+            } finally {
+                setLoading(false);
+            }
+        }
+    }, [currentUser, urlAttractionId]);
+    */
+
+    useEffect(() => {
+        const fetchAttractions = async () => {
+            try {
+                const response = await axios.get("/api/enclosures", {
+                    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+                });
+                setEnclosureList(response.data);
+            } catch (err) {
+                console.error("Error fetching enclosures list:", err);
+            }
+        };
+        if (currentUser && currentUser.staffRole === "Manager") {
+            fetchEnclosures();
+        }
+    }, [currentUser]);
+
+    
 
     // handle changes in form inputs
     const handleChange = (a) => {
