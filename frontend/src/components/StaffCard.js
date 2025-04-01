358ebedb26f53a0e7240916a50f244d4c88ac660
Jalvz1 <javieralvz0427@gmail.com>
2025-04-01 00:32:25 -0500

    StaffCard component tested and works as expected

--- frontend/src/components/StaffCard.js
+++ frontend/src/components/StaffCard.js
@@ -7,7 +7,7 @@ const StaffCard = ({ staff }) => {
     return (
         <div className="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300">
             <div className="p-4">
-                <h3 className="text-xl font-semibold mb-2 text-gray-800 font-['Mukta_Mahee']">{staff.Name}</h3>
+                <h3 className="text-xl font-semibold mb-2 text-gray-800 font-['Mukta_Mahee']">{staff.NAME}</h3>
 
                 <div className="text-sm text-gray-500 font-['Lora']">
                     Staff ID: {staff.StaffID}
@@ -16,15 +16,15 @@ const StaffCard = ({ staff }) => {
                 {/* If manager show show they are manager next to their role */}
                 <div className="mb-2 flex items-center text-gray-600 font-['Lora']">
                     <User className="w-4 h-4 mr-2" />
-                    Role: {staff.staffType} {staff.staffRole === 'Manager' && '(Manager)'} 
+                    Role: {staff.StaffType} {staff.Role === 'Manager' && '(Manager)'} 
                 </div>
 
                 <div className="text-sm text-gray-500 font-['Lora']">
-                    Staff Type: {staff.staffType}
+                    Staff Type: {staff.StaffType}
                 </div>
 
                 <div className="text-sm text-gray-500 font-['Lora']">
-                    Role: {staff.staffRole}
+                    Role: {staff.Role}
                 </div>
 
                 <div className="text-sm text-gray-500 font-['Lora']">
